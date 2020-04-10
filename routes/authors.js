63a52a95e1d0427d77de751d40ac0eb92cc20ffc
Mina Hozien <msaid@outlook.com.au>
2020-04-11 09:18:16 +1000

    test 2

--- routes/authors.js
+++ routes/authors.js
@@ -4,21 +4,25 @@ const Author = require('../models/author')
 
 // ALL Authors Route
 router.get('/', async (req, res) => {
-    let searchOptions = {}
-    if (req.query.name != null && req.query.name !=='') {
-        searchOptions.name = new RegExp(req.query.name,'i')
-    }
-    try {
-        const authors = await Author.find(searchOptions)
-        res.render('authors/index',{
-            authors : authors,
-            searchOptions: req.query
-        })
-    } catch (error) {
-        res.redirect('/')
-    }
-    
+res.render('authors/index')
 })
+
+// router.get('/', async (req, res) => {
+//     let searchOptions = {}
+//     if (req.query.name != null && req.query.name !=='') {
+//         searchOptions.name = new RegExp(req.query.name,'i')
+//     }
+//     try {
+//         const authors = await Author.find(searchOptions)
+//         res.render('authors/index',{
+//             authors : authors,
+//             searchOptions: req.query
+//         })
+//     } catch (error) {
+//         res.redirect('/')
+//     }
+    
+// })
 
 // New Author Route
 router.get('/new', (req, res) => {
