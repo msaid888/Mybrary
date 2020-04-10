8c19574520140b0a4143015f8cac8d9eb89fade2
Mina Hozien <msaid@outlook.com.au>
2020-04-11 09:23:31 +1000

    final not working on Heroku

--- routes/authors.js
+++ routes/authors.js
@@ -3,26 +3,22 @@ const router = express.Router()
 const Author = require('../models/author')
 
 // ALL Authors Route
-router.get('/', (req, res) => {
-res.render('authors/index')
-})
-
-// router.get('/', async (req, res) => {
-//     let searchOptions = {}
-//     if (req.query.name != null && req.query.name !=='') {
-//         searchOptions.name = new RegExp(req.query.name,'i')
-//     }
-//     try {
-//         const authors = await Author.find(searchOptions)
-//         res.render('authors/index',{
-//             authors : authors,
-//             searchOptions: req.query
-//         })
-//     } catch (error) {
-//         res.redirect('/')
-//     }
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !=='') {
+        searchOptions.name = new RegExp(req.query.name,'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index',{
+            authors : authors,
+             searchOptions: req.query
+        })
+     } catch (error) {
+         res.redirect('/')
+     }
     
-// })
+ })
 
 // New Author Route
 router.get('/new', (req, res) => {
