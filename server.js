82d587872849255385f0dc66fb71f8a84f79db96
Mina Hozien <msaid@outlook.com.au>
2020-04-11 10:02:34 +1000

    final not working on Heroku

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
-app.set('views', __dirname+'/views/')
+app.set('views', __dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({limit:'10mb',extended: false}))
 
 app.use('/',indexRouter)
-app.use('/authors',authorRouter)
+app.use('/authors/',authorRouter)
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{
